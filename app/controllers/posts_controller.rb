41499f278e364c39c90cbf021b2daf02d365d0e0
momomo0505 <techcamp.fujita.t@gmail.com>
2023-05-07 21:24:48 +0900

    計算機能の準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,10 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.order(id: "DESC")
   end
 
-  def new
-  end
-
-  def create
-    Post.create(content: params[:content])
-  end
+def create
+  Post.create(content: params[:content])
+  redirect_to action: :index
 end
